26e12d1d134ff7231785a64fb6148349a46af308
bippan <bippan@pop-os.localdomain>
2022-02-22 21:32:44 +0530

    updating css for home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
-import { Container } from './Styles/Container.style';
-import {Styledhed,Images} from '../Components/Styles/Home.style'
+import React from "react";
+import { Container } from "./Styles/Container.style";
+import { Styledhed, Images } from "../Components/Styles/Home.style";
 
-const Home=()=>{
-    return(
-        <>
-       <Container width="100%">
-        <Container width='100%'>
-          <Styledhed width='500px'>
-          <h1>Hi, I am John,<br/>Creative Technologist</h1>
-          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet<br/>
-          sint. Velit officia consequat duis enim velit mollit. Exercitation veniam<br/>
-         consequat sunt nostrud amet.</p>
-         <br/>
-         <button className='btn'  style={{background:'orange',Color:'black',padding:'10px',border:'none'}}>Download Resume</button>
-         </Styledhed>
-          <Images src='https://crowdfavorite.com/wp-content/uploads/2020/04/web-design-integration-twitter.jpg' alt='sd'/>
-          
-          
-         
-
+const Home = () => {
+  return (
+    <>
+      <Container width="100%" display="flex" flexDirection="row" justifyContent='center'>
+        <Container width="600px">
+          {/* <Styledhed width='500px'> */}
+          <h1>
+            Hi, I am John,
+            <br />
+            Creative Technologist
+          </h1>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            <br />
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam
+            <br />
+            consequat sunt nostrud amet.
+          </p>
+          <br />
+          <button
+            className="btn"
+            style={{
+              background: "orange",
+              Color: "black",
+              padding: "10px",
+              border: "none",
+            }}
+          >
+            Download Resume
+          </button>
+          {/* </Styledhed> */}
         </Container>
-       </Container>
 
-        </>
-    )
-}
+        <Container width="200px">
+          <Images
+            src="https://crowdfavorite.com/wp-content/uploads/2020/04/web-design-integration-twitter.jpg"
+            alt="sd"
+          />
+        </Container>
+      </Container>
+    </>
+  );
+};
 export default Home;
